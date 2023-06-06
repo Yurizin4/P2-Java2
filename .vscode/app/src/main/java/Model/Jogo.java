04d811fc954c83acf49acb3e50a14d7f440d3efa
Francieli Martins <fmartinsPOW777@gmail.com>
2023-06-06 18:26:20 +0000

    commit p2

--- .vscode/app/src/main/java/Model/Jogo.java
+++ .vscode/app/src/main/java/Model/Jogo.java
@@ -1,51 +1,39 @@
-package Model;
+package application.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="Jogo")
+@Table(name="jogo")
 public class Jogo {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
     private String titulo;
     private int anoDeLancamento;
 
-    @ManyToOne
+
     public int getId() {
         return id;
     }
     public void setId(int id) {
         this.id = id;
     }
-    
-    
+
     public String getTitulo() {
         return titulo;
     }
     public void setTitulo(String titulo) {
         this.titulo = titulo;
-    }
 
+   }    
     public int getAnoDeLancamento() {
-        return anoDeLancamento;
+     return anoDeLancamento;
     }
     public void setAnoDeLancamento(int anoDeLancamento) {
         this.anoDeLancamento = anoDeLancamento;
-    }
-
-
-    
-
-
-
-    
-
-    
-}
+    } 
+  }
